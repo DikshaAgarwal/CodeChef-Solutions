f160543edf2e580e93d31c960c7fb0ecc0a8f76d
Niwesh Gupta <niweshgupta@gmail.com>
2018-10-02 23:45:49 +0530

    Test.cpp added

---
+++ TEST.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+
+	int main()
+{	
+	while(1)
+	{
+		int k;
+		cin>>k;
+		if(k!=42)
+			cout<<k<<endl;
+		else 
+			return 0;
+	}
+	return 0;
+}
+
+
